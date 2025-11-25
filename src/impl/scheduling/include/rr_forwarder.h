6e0f699e231a0937e5b63fb3a50426a48f99ad93
Crunchy0 <romapavlov2001@mail.ru>
2025-11-26 00:06:20 +0300

    Added round-robin scheduler

---
+++ src/impl/scheduling/include/rr_forwarder.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "event.h"
+#include "edr_logger.h"
+
+#include "endpoints/include/endpoint_impl.h"
+
+#include "client_request.h"
+#include "server_response.h"
+#include "forwarder.h"
+
+#include <future>
+#include <memory>
+#include <mutex>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+
+namespace utf
+{
+namespace scheduling
+{
+
+class rr_forwarder : public forwarder
+{
+private:
+    struct pending_request
+    {
+        uint64_t request_id;
+        uint32_t listener_id;
+        uint16_t client_port;
+        uint16_t server_port;
+        boost::asio::ip::address_v4 client_addr;
+        boost::asio::ip::address_v4 server_addr;
+        uint64_t arrival_time_ms;
+        uint64_t fwd_time_ms;
+    };
+
+public:
+    rr_forwarder() = delete;
+    rr_forwarder(std::vector<std::shared_ptr<utf::endpoints::tcp_client>>&& clients);
+    ~rr_forwarder() override;
+    
+    void schedule(const client_request& req) override;
+    void schedule(client_request&& req) override;
+    
+    event<uint16_t, boost::asio::ip::address_v4, uint16_t, const std::vector<char>&> send_back_evt;
+    event<const aux::edr&> edr_report_evt;
+    
+private:
+    void accept_response(const server_response& response);
+    void forward_requests();
+    void send_responses();
+    
+    void main_loop();
+    
+    std::vector<std::shared_ptr<endpoints::tcp_client>> m_clients;
+    std::unordered_map<uint64_t, pending_request> m_pending_reqs;
+    std::deque<client_request> m_requests;
+    std::deque<server_response> m_responses;
+    
+    std::mutex m_req_mx;
+    std::mutex m_resp_mx;
+    std::mutex m_pend_mx;
+    
+    std::future<void> m_stop_sync;
+    std::atomic_bool m_is_stopped;
+    
+    decltype(m_clients)::iterator get_next_client();
+
+    decltype(m_clients)::iterator m_curr_client;
+};
+
+}
+}
---
+++ src/impl/scheduling/source/rr_forwarder.cpp
@@ -0,0 +1,206 @@
+#include "rr_forwarder.h"
+
+#include <chrono>
+#include <exception>
+#include <random>
+#include <thread>
+#include <limits>
+
+using uint64_t_lim = std::numeric_limits<uint64_t>;
+
+static std::random_device rand_dev;
+static std::default_random_engine rand_eng(rand_dev());
+static std::uniform_int_distribution<uint64_t> id_distr(uint64_t_lim::min(), uint64_t_lim::max());
+
+namespace utf
+{
+namespace scheduling
+{
+
+rr_forwarder::rr_forwarder(std::vector<std::shared_ptr<utf::endpoints::tcp_client>>&& clients) :
+    m_clients(clients),
+    m_curr_client(m_clients.begin())
+{
+    if(m_clients.empty())
+        throw std::runtime_error("rr_forwarder: Empty clients list");
+    
+    for(const auto& cl : m_clients)
+    {
+        cl->resp_giveaway_evt.subscribe(this, &rr_forwarder::accept_response);
+    }
+
+    m_stop_sync = std::async(
+        &rr_forwarder::main_loop,
+        this
+    );
+}
+
+rr_forwarder::~rr_forwarder()
+{
+    m_is_stopped.store(true);
+    m_stop_sync.wait();
+
+    {
+        std::scoped_lock l(m_pend_mx, m_req_mx, m_resp_mx);
+    }
+
+    for(const auto& pr : m_pending_reqs)
+    {
+        // Build edr log
+        aux::edr edr
+        {
+            .arrival_time_ms = pr.second.arrival_time_ms,
+            .tcp_resp_dur_ms = aux::edr::TIMEOUT,
+            .client_addr = pr.second.client_addr,
+            .server_addr = pr.second.server_addr,
+            .client_port = pr.second.client_port,
+            .server_port = pr.second.server_port
+        };
+
+        edr_report_evt.invoke(edr);
+    }
+}
+
+void rr_forwarder::schedule(const client_request& req)
+{
+    std::lock_guard l(m_req_mx);
+    m_requests.push_back(req);
+}
+
+void rr_forwarder::schedule(client_request&& req)
+{
+    std::lock_guard l(m_req_mx);
+    m_requests.push_back(std::move(req));
+}
+
+void rr_forwarder::accept_response(const server_response& response)
+{
+    std::lock_guard l (m_resp_mx);
+    m_responses.push_back(response);
+}
+
+decltype(rr_forwarder::m_clients)::iterator rr_forwarder::get_next_client()
+{
+    auto it = m_curr_client + 1;
+    for(;it != m_curr_client;)
+    {
+        if(it == m_clients.end())
+        {
+            it = m_clients.begin();
+            continue;
+        }
+
+        if(!it->get()->is_connected())
+        {
+            ++it;
+            continue;
+        }
+
+        m_curr_client = it;
+        return m_curr_client;
+    }
+    return m_clients.end();
+}
+
+void rr_forwarder::forward_requests()
+{
+    std::lock_guard l1(m_req_mx);
+    while(!m_requests.empty())
+    {
+        if(m_curr_client == m_clients.end())
+            m_curr_client = m_clients.begin();
+
+        auto& req = m_requests.front();
+
+        auto it = get_next_client();
+        if(it == m_clients.end())
+            return;
+        
+
+        uint64_t rid;
+        {
+            std::lock_guard l2(m_pend_mx);
+            do
+            {
+                rid = id_distr(rand_eng);
+            } while (m_pending_reqs.contains(rid));
+
+            using namespace chrono;
+            uint64_t current_time_ms =
+                duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+            pending_request pr
+            {
+                .request_id = rid,
+                .listener_id = req.listener_id,
+                .client_port = req.client_port,
+                .server_port = it->get()->get_port(),
+                .client_addr = req.client_addr,
+                .server_addr = it->get()->get_address(),
+                .arrival_time_ms = req.arr_timestamp_ms,
+                .fwd_time_ms = current_time_ms
+            };
+            m_pending_reqs.emplace(rid, pr);
+        }
+        it->get()->send(rid, req.payload.begin(), req.payload.end());
+        m_requests.pop_front();
+    }
+}
+
+void rr_forwarder::send_responses()
+{
+    std::lock_guard l(m_resp_mx);
+    while(!m_responses.empty())
+    {
+        const auto& resp = m_responses.front();
+
+        pending_request pr;
+        {
+            std::lock_guard l(m_pend_mx);
+            if(!m_pending_reqs.contains(resp.request_id))
+            {
+                m_responses.pop_front();
+                continue;
+            }
+
+            auto it = m_pending_reqs.find(resp.request_id);
+            pr = it->second;
+            m_pending_reqs.erase(it);
+        }
+
+        aux::edr edr
+        {
+            .arrival_time_ms = pr.arrival_time_ms,
+            .tcp_resp_dur_ms = resp.resp_timestamp_ms - pr.fwd_time_ms,
+            .client_addr = pr.client_addr,
+            .server_addr = pr.server_addr,
+            .client_port = pr.client_port,
+            .server_port = pr.server_port
+        };
+        
+        send_back_evt.invoke(pr.listener_id, pr.client_addr, pr.client_port, resp.payload);
+        edr_report_evt.invoke(edr);
+        m_responses.pop_front();
+    }
+}
+
+void rr_forwarder::main_loop()
+{
+    for(;;)
+    {
+        if(m_is_stopped.load())
+            break;
+        
+        forward_requests();
+        send_responses();
+        
+        std::this_thread::yield();
+    }
+
+    for(const auto& cl : m_clients)
+    {
+        cl->resp_giveaway_evt.unsubscribe(this, &rr_forwarder::accept_response);
+    }
+}
+
+}
+}
