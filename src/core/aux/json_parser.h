86a127822543361956e9593194b4fdc875e8e55d
Crunchy0 <romapavlov2001@mail.ru>
2025-11-26 00:01:42 +0300

    Fixed Boost::json linker errors

--- src/core/aux/json_parser.h
+++ src/core/aux/json_parser.h
@@ -1,52 +1,18 @@
 #pragma once
 
-#include <boost/json/src.hpp>
+#include <boost/json.hpp>
 
 #include <fstream>
-
-namespace json = boost::json;
+#include <optional>
 
 namespace utf
 {
 namespace aux
 {
 
-std::optional<json::value> parse_json(const std::string& fn)
-{
-    static json::stream_parser parser;
+static boost::json::stream_parser parser;
 
-    std::ifstream ifs(fn);
-    if(!ifs.is_open())
-    {
-        return std::nullopt;
-    }
-
-    boost::system::error_code ec;
-    parser.reset();
-
-    std::string line;
-    while(std::getline(ifs, line))
-    {
-        parser.write(line, ec);
-        if(ec)
-        {
-            return std::nullopt;
-        }
-    }
-    
-    parser.finish(ec);
-    if(ec)
-    {
-        return std::nullopt;
-    }
-    
-    json::value config = parser.release();
-    if(!config.is_object())
-    {
-        return std::nullopt;
-    }
-    return config;
-}
+std::optional<boost::json::value> parse_json(const std::string& fn);
 
 }
 }
